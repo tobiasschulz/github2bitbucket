966fc4136f5c6673903b5882f8a6f24b28bfd077
PSE Knot³ <uaeti@student.kit.edu>
2014-04-20 01:11:23 +0200

    initial commit

---
+++ config.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+
+our @config = (
+
+	{
+		github_user => tobiasschulz,
+		bitbucket_user => tobiasschulz,
+		private => 0,
+		repos => [
+			"github2bitbucket",	"scripts",			"uni-betriebssysteme",
+			"uni-swt",		"rezepte",			"schule",
+			"flickrsmartsync",	"tobiasschulz.github.io",	"limelight-pc-opengl",
+			"LaTeX-examples",	"SDL2-CS",			"openal-test",
+			"doxygen",		"call-qt",			"addic7ed-ruby",
+			"voipcall",		"private",			"tailserver",
+			"craftbook",		"gimp-plugins",			"fdupes",
+			"android-vnc",
+		],
+	},
+
+);
---
+++ github2bitbucket.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+sub sync_repo {
+	my $p = {@_};
+	my $github_user = $p->{github_user};
+	my $bitbucket_user = $p->{bitbucket_user};
+	my $private = $p->{private};
+	my $repo_name = $p->{repo_name};
+	my $base_dir = $p->{base_dir};
+	
+	print "Sync $github_user/$repo_name to $bitbucket_user/$repo_name (private=".$private.")\n";
+
+	chdir $base_dir;
+	system q( test -d ).$repo_name.q( || git clone git@github.com:).$github_user.q(/).$repo_name.q(.git );
+	chdir $base_dir."/".$repo_name and do {
+		print "Pull all branched from github\n";
+		system q( bash -c "git clean -xdf; git fetch --all; for i in $(git branch | sed 's/^.//'); do git checkout $i; git pull; done" );
+		system q( git remote add bitbucket git@bitbucket.org:).$bitbucket_user.q(/).$repo_name.q(.git );
+		print "Push all branched to bitbucket\n";
+		system q( git push bitbucket --all );
+	};
+
+	select undef, undef, undef, 1;
+}
+
+our @config;
+require "config.pl";
+
+my $base_dir = $ENV{HOME}."/tmp-github2bitbucket";
+system qq( mkdir -p $base_dir );
+print "base directory: ".$base_dir."\n";
+
+foreach my $packed (@config) {
+	foreach my $repo (@{$packed->{repos}}) {
+		sync_repo (%$packed, repos => undef, repo_name => $repo, base_dir => $base_dir);
+	}
+}
